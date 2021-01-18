2267f3e19c90552082de83ad5d889ef387ab7ca7
sezniv <sezniv@gmail.com>
2021-01-18 16:00:45 -0300

    Se agrega funcion para mostar Transportista y Alumnos

--- Mo6 indi5/migrate_01/urls.py
+++ Mo6 indi5/migrate_01/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('asignatura/', views.asignaturax , name = 'asignatura'),
     path('profesor/', views.profesorx , name = 'profesor' ),
     path('alumno/', views.mostrar_alumnos, name = 'alumno'),
-    path('lista_asignatura/', views.ListarAsignatura.as_view(), name = 'lista_asignatura')
+    path('lista_asignatura/', views.ListarAsignatura.as_view(), name = 'lista_asignatura'),
+    path('add_transportista/', views.add_transportista, name ='add_transportista'),
+    path('ver_alumnos/', views.mostrar_alumnos, name = 'ver_alumnos'),
+    path('ver_transportistas/', views.mostar_transportistas, name ='ver_transportistas'),
 ]
--- Mo6 indi5/migrate_01/views.py
+++ Mo6 indi5/migrate_01/views.py
@@ -21,6 +21,15 @@ def asignaturax(request):
     context = {'asig': asignatura}    
     return render(request, 'migrate_01/asignatura.html', context)
 
+def add_transportista(request):
+    for i in range(0,5):
+        transportista = Transportista(nombre= 'Claudio',
+        patente= 'AZDB12'
+        )
+    transportista.save()
+    transportista = Transportista.objects.values()
+    context = {'transportista': transportista}
+    return render(request, 'migrate_01/transportistas.html', context)
 
 def profesorx(request):
     for i in range(0,5):
@@ -35,11 +44,17 @@ def profesorx(request):
     context = {'profe': profe}
     return render(request, 'migrate_01/profesor.html', context)
 
-
+#MUESTRA ALUMNOS
 def mostrar_alumnos(request):
-    listar_alumnos = list(Alumno.objects.all())
-    context = {'alumnos':listar_alumnos}
-    return render(request,'migrate_01/alumno.html', context= context)
+    listar_alumnos = list(Alumno.objects.all().values())
+    datos = {'alumnos': listar_alumnos}
+    return render(request,'migrate_01/ver_alumnos.html', context= datos)
+
+#MUESTRA TRANSPORTISTAS
+def mostar_transportistas(request):
+    lista_transportista = list(Transportista.objects.all().values())
+    datos = {'transportistas': lista_transportista}
+    return render(request,'migrate_01/ver_transportistas.html', context= datos)
 
 class CrearAsignatura(CreateView):
     model= AsignaturaFormularioForms 
@@ -59,6 +74,6 @@ class EditarAsignatura(UpdateView):
 class ListarAsignatura(ListView):
     model= AsignaturaFormularioForms 
     fields= '__all__'
-    success_url = reverse_lazy('migrate_01:asignatura')
+    success_url = reverse_lazy('migrate_01:lista_asignatura')
 
 
